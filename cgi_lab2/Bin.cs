23110cf794608ca5f56bf0c01b9f6981b47da2bc
Nick <nickitrytry@gmail.com>
2025-04-28 13:20:18 +0300

    first commit

---
+++ cgi_lab2/Bin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cgi_lab2
+{
+     internal class Bin
+     {
+          public static int X, Y, Z;
+          public short[] arr;
+          public Bin() { }
+          public void readBin(string path)
+          {
+               if (File.Exists(path))
+               {
+                    BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+                    X = reader.ReadInt32();
+                    Y = reader.ReadInt32();
+                    Z = reader.ReadInt32();
+
+                    int arr_size = X * Y * Z;
+
+                    arr = new short[arr_size];
+                    for(int i = 0; i < arr_size; i++)
+                    {
+                         arr[i] = reader.ReadInt16();
+                    }
+               }
+          }
+     }
+}
---
+++ cgi_lab2/View.cs
@@ -0,0 +1,26 @@
+﻿
+using OpenTK.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace cgi_lab2
+{
+     internal class View
+     {
+          public void setupView()
+          {
+
+               GL.ShadeModel(ShadingModel.Smooth);
+               GL.MatrixMode(MatrixMode.Projection);
+               GL.LoadIdentity();
+               GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+               GL.Viewport(0, 0, 0, 0);
+          }
+
+          public void drawQuads()
+     }
+}
